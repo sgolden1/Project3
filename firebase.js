e6b1b0d61e1b090fbb753e54056232789b0c1205
sgolden1 <sgolden8@villanova.edu>
2023-12-05 20:44:58 -0500

    Add app

---
+++ firebase.js
@@ -0,0 +1,27 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBq9LJcIafbb0844Djpm00Ev6lvlB5CF9w',
+  authDomain: 'novaconnect-f516f.firebaseapp.com',
+  projectId: 'novaconnect-f516f',
+  storageBucket: 'gs://novaconnect-f516f.appspot.com',
+  messagingSenderId: '556653428642',
+  appId: 'AIzaSyBq9LJcIafbb0844Djpm00Ev6lvlB5CF9w',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}import * as firebase from firebase;
+
+const firebaseConfig = {
+  apiKey: AIzaSyBq9LJcIafbb0844Djpm00Ev6lvlB5CF9w,
+  authDomain: novaconnect-f516f.firebaseapp.com,
+  projectId: novaconnect-f516f,
+  storageBucket: gs://novaconnect-f516f.appspot.com,
+  messagingSenderId: 556653428642,
+  appId: AIzaSyBq9LJcIafbb0844Djpm00Ev6lvlB5CF9w,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
